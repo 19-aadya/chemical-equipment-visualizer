be333240e776cf54e45b342acd0aa2af2cdd7c9e
prochauhan <aadya.chauhan.19@gmail.com>
2026-01-27 19:12:57 +0530

    Fix Django app path for equipment

--- backend/server/equipment/apps.py
+++ backend/server/equipment/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class EquipmentConfig(AppConfig):
-    name = 'equipment'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'server.equipment'
--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'equipment',
+    'server.equipment',
 ]
 
 MIDDLEWARE = [
